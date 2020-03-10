b7209f567657bda99a6681f03afd44ab1214a2c0
Akash Katakam <katakam.a@husky.neu.edu>
2020-03-10 13:37:33 -0400

    changed pom.xml

--- src/main/java/com/allstars/recipie_management_system/entity/Recipie.java
+++ src/main/java/com/allstars/recipie_management_system/entity/Recipie.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Set;
 
 @Entity
-@JsonIgnoreProp    /erties(value={"user"}, allowSetters= true)
+@JsonIgnoreProperties(value={"user"}, allowSetters= true)
 
 public class Recipie {
 
